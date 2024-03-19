e4646e13a01110612a33c498aaa2b1f4d82bfd92
Aleksey Semenov <lexa140191@gmail.com>
2024-03-19 19:00:44 +0300

    Chapter 1 add exercises

---
+++ Chapter01 Why data structures matter/exercises.py
@@ -0,0 +1,37 @@
+"""
+1. For an array containing 100 elements, provide the number of steps the following operations would take:
+    a. Reading
+    b. Searching for a value not contained within the array
+    c. Insertion at the beginning of the array
+    d. Insertion at the end of the array
+    e. Deletion at the beginning of the array
+    f. Deletion at the end of the array
+
+    a - 1
+    b - 100
+    c - 101
+    d - 1
+    e - 100
+    f - 1
+
+
+2. For an array-based set containing 100 elements, provide the number of steps the following operations would take:
+    a. Reading
+    b. Searching for a value not contained within the array
+    c. Insertion of a new value at the beginning of the set
+    d. Insertion of a new value at the end of the set
+    e. Deletion at the beginning of the set
+    f. Deletion at the end of the set
+
+    a - 1
+    b - 100
+    c - 201
+    d - 101
+    e - 100
+    f - 1
+
+
+3. Normally the search operation in an array looks for the first instance of a given value, But sometimes we may to look every instance of a given value. For example, says we want to count how many times the value "apple" is found inside an array. How many steps would it take to find all the "apples"? Give your answer in terms of N.
+
+    N steps
+"""
